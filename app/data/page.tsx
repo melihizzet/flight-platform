f1f33894f1a2beeab29742740d6f852bd7d2517f
melihizzet <melihizzetzorluoglu@gmail.com>
2026-05-28 11:39:03 +0000

    fixed data page

---
+++ app/data/page.tsx
@@ -0,0 +1,11 @@
+export default function DataPage() {
+  return (
+    <main className="min-h-screen bg-[#020617] text-white flex items-center justify-center">
+
+      <h1 className="text-6xl font-black">
+        Data Page
+      </h1>
+
+    </main>
+  );
+}
