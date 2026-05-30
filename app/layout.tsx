8889e27bc04f80308a78ef4ad0ce20b21cfb6c53
melihizzet <melihizzetzorluoglu@gmail.com>
2026-05-30 10:10:09 +0000

    google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import "./globals.css";
 export const metadata = {
   title: "UçGit",
   description: "Ucuz uçak bileti bul",
+
+  verification: {
+    google: "IXpXj097D9wBObuiiVN30qR4NoamhlRhOtOR1b3fV50",
+  },
 };
 
 export default function RootLayout({
