ac6b9a7237dc3e4a55a826537ff1b5908d4915dd
melihizzet <melihizzetzorluoglu@gmail.com>
2026-05-30 10:37:53 +0000

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.ucgit.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.ucgit.com/blog",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.ucgit.com/campaigns",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.ucgit.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.ucgit.com/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.ucgit.com/faq",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.ucgit.com/privacy-policy",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.ucgit.com/terms",
+      lastModified: new Date(),
+    },
+  ];
+}
